6e45988f7e2ff054a8c337dcd1d0a867e305c63a
David Poulton <davidpoulton123@hotmail.co.uk>
2020-04-30 00:09:16 +0100

    -animated burger menu on open

--- script.js
+++ script.js
@@ -68,6 +68,9 @@ function swipeLeft() {
 let burger = document.querySelector(".burger-menu");
 let slideMenu = document.querySelector(".slide-menu");
 let page = document.querySelector(".page");
+let burgerDiv1 = document.querySelector(".burger-div1")
+let burgerDiv2 = document.querySelector(".burger-div2")
+let burgerDiv3 = document.querySelector(".burger-div3")
 
 let menuOpacity = window.getComputedStyle(slideMenu);
 
@@ -77,11 +80,17 @@ function toggleMenu() {
         slideMenu.classList.remove("hide");
         slideMenu.classList.add("slide-menu-right");
         page.classList.add("dark");
+        burgerDiv1.style.transform = "translateY(-5px)";
+        burgerDiv2.style.transform = "translate(10px)";
+        burgerDiv3.style.transform = "translateY(5px)";
     } else if (menuOpacity.opacity === "1") {
         slideMenu.classList.add("hide");
         slideMenu.classList.remove("show");
         slideMenu.classList.remove("slide-menu-right");
         page.classList.remove("dark");
+        burgerDiv1.style.transform = "translateY(0px)";
+        burgerDiv2.style.transform = "translate(0px)";
+        burgerDiv3.style.transform = "translateY(0px)";
     }
 }
 
